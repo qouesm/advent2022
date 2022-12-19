37e75a6e7ee6c2e75714f50bf616176b3abbf5b5
qouesm <qouesm@tuta.io>
2022-12-19 17:10:12 -0500

    day 6 remove prints

--- 6.go
+++ 6.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/qouesm/advent2022/read"
 )
 
@@ -14,19 +12,18 @@ func q6a() int {
 	// find valid
 	for i, v := range input[4:] {
 		carriage = carriage[1:] + string(v)
-    match := false
+		match := false
 		for i := 0; i < len(carriage); i++ {
 			for j := i + 1; j < len(carriage); j++ {
 				if carriage[i] == carriage[j] {
-          match = true
-          break
+					match = true
+					break
 				}
 			}
 		}
-    if !match {
-      fmt.Println(carriage)
-      return i+5 // +1 for one-indexing; +4 to get to end of carriage
-    }
+		if !match {
+			return i + 5 // +1 for one-indexing; +4 to get to end of carriage
+		}
 	}
 
 	return -1
@@ -40,19 +37,18 @@ func q6b() int {
 	// find valid
 	for i, v := range input[14:] {
 		carriage = carriage[1:] + string(v)
-    match := false
+		match := false
 		for i := 0; i < len(carriage); i++ {
 			for j := i + 1; j < len(carriage); j++ {
 				if carriage[i] == carriage[j] {
-          match = true
-          break
+					match = true
+					break
 				}
 			}
 		}
-    if !match {
-      fmt.Println(carriage)
-      return i+15 // +1 for one-indexing; +15 to get to end of carriage
-    }
+		if !match {
+			return i + 15 // +1 for one-indexing; +15 to get to end of carriage
+		}
 	}
 
 	return -1
