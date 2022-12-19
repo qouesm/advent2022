7ae24ebfd288d6240fc1be49eaac6c27e24a6405
qouesm <qouesm@tuta.io>
2022-12-19 16:41:56 -0500

    day 6

---
+++ 6.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/qouesm/advent2022/read"
+)
+
+func q6a() int {
+	input := read.ReadFile("input/6.txt")
+	// initial sliding carriage
+	carriage := input[:4]
+
+	// find valid
+	for i, v := range input[4:] {
+		carriage = carriage[1:] + string(v)
+    match := false
+		for i := 0; i < len(carriage); i++ {
+			for j := i + 1; j < len(carriage); j++ {
+				if carriage[i] == carriage[j] {
+          match = true
+          break
+				}
+			}
+		}
+    if !match {
+      fmt.Println(carriage)
+      return i+5 // +1 for one-indexing; +4 to get to end of carriage
+    }
+	}
+
+	return -1
+}
+
+func q6b() int {
+	input := read.ReadFile("input/6.txt")
+	// initial sliding carriage
+	carriage := input[:14]
+
+	// find valid
+	for i, v := range input[14:] {
+		carriage = carriage[1:] + string(v)
+    match := false
+		for i := 0; i < len(carriage); i++ {
+			for j := i + 1; j < len(carriage); j++ {
+				if carriage[i] == carriage[j] {
+          match = true
+          break
+				}
+			}
+		}
+    if !match {
+      fmt.Println(carriage)
+      return i+15 // +1 for one-indexing; +15 to get to end of carriage
+    }
+	}
+
+	return -1
+}
--- main.go
+++ main.go
@@ -11,5 +11,6 @@ func main() {
 	fmt.Println("Day 3", q3a(), q3b())
 	fmt.Println("Day 4", q4a(), q4b())
 	fmt.Println("Day 5", q5a(), q5b())
+	fmt.Println("Day 6", q6a(), q6b())
 
 }
